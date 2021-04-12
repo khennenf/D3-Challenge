9dc05698f6e92cf22fc88b612befa1d9a032cffe
Keri <khennenf@yahoo.com>
2021-04-12 14:15:17 -0400

    chart

--- for_terminal/D3_data_journalism/assets/js/app.js
+++ for_terminal/D3_data_journalism/assets/js/app.js
@@ -21,14 +21,82 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-//Import Data
-data = d3.csv("assets/data/data.csv")
-console.log(data)
+  d3.csv("assets/data/data.csv").then(function(theData) {
 
-d3.csv("assets/data/data.csv").then(function(data) {
-    data.income = +data.income;
-    data.obesity = +data.obesity
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    theData.forEach(function(data) {
+      data.income = +data.income;
+      data.obesity = +data.obesity;
+    });
+
+
+//Create scale functions
+var xLinearScale = d3.scaleLinear()
+.domain([20000, d3.max(theData, d => d.income)])
+.range([0, width]);
+
+var yLinearScale = d3.scaleLinear()
+.domain([20, d3.max(theData, d => d.obesity)])
+.range([height, 0]);
+
+//Create axis functions
+var bottomAxis = d3.axisBottom(xLinearScale);
+var leftAxis = d3.axisLeft(yLinearScale);
+
+//Append Axes to the chart
+chartGroup.append("g")
+.attr("transform", `translate(0, ${height})`)
+.call(bottomAxis);
+
+chartGroup.append("g")
+.call(leftAxis);
+
+//Create Circles
+var circlesGroup = chartGroup.selectAll("circle")
+.data(theData)
+.enter()
+.append("circle")
+.attr("cx", d => xLinearScale(d.income))
+.attr("cy", d => yLinearScale(d.obesity))
+.attr("r", "15")
+.attr("fill", "lightskyblue")
+.attr("opacity", "1");
+
+//Create tool tip
+var toolTip = d3.tip()
+.attr("class", "tooltip") //style
+.offset([80, -60]) //placement
+.html(function(d) { //structure/content
+  return (`${d.abbr}<br>Income: ${d.income}<br>Obesity Rate: ${d.obesity}`);
 });
 
+//Create tooltip in the chart
+chartGroup.call(toolTip)
+
+//Create event listeners to display
+circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data, this);
+  })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
 
 
+chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Obese (%)");
+
+chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Household Income Median ($)");
+
+  }).catch(function(error) {
+    console.log(error);
+  });
